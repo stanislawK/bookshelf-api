bec6567aaf468b57e86af5707aaa330b00de7b85
stanislawK <stanislaw.krupienko@gmail.com>
2019-06-27 23:50:35 +0200

    Added category model

--- api/base/models.py
+++ api/base/models.py
@@ -7,3 +7,10 @@ class AuthorModel(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- api/base/tests/test_models.py
+++ api/base/tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from base.models import AuthorModel
+from base.models import AuthorModel, CategoryModel
 
 
 @pytest.mark.django_db
@@ -12,3 +12,11 @@ class TestModels():
 
         assert str(author) == author.name
         assert authors_db[0] == author
+
+    def test_new_category(self):
+        """Test creating a new category"""
+        category = CategoryModel.objects.create(name="Fiction")
+        category_db = CategoryModel.objects.all()
+
+        assert str(category) == category.name
+        assert category_db[0] == category
